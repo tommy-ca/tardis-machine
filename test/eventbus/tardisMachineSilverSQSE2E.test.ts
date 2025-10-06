ff23a68c1231de78daeb9760a84aa3163e6f0e99
Tommy K <140900186+tommy-ca@users.noreply.github.com>
2025-10-06 08:02:45 +0200

    Change E2E test ports to avoid conflicts

--- test/eventbus/tardisMachineSilverSQSE2E.test.ts
+++ test/eventbus/tardisMachineSilverSQSE2E.test.ts
@@ -8,7 +8,7 @@ import { TradeRecordSchema, Origin } from '../../src/generated/lakehouse/silver/
 
 jest.setTimeout(240000)
 
-const PORT = 8102
+const PORT = 8202
 const HTTP_REPLAY_NORMALIZED_URL = `http://localhost:${PORT}/replay-normalized`
 const queueName = 'silver-events-e2e'
 const cacheDir = './.cache-silver-sqs-e2e'
